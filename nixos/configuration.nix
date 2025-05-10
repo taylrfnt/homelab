d84a7c076622d1c730d18c632aadf6ee7ea408b0
taylrfnt <43214679+taylrfnt@users.noreply.github.com>
2025-05-10 17:18:51 -0500

    introduce sops for secret mgmt & do some cleanup (#6)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,31 +1,51 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, meta, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-    ];
+  config,
+  lib,
+  pkgs,
+  meta,
+  ...
+}: {
+
+  sops = {
+    defaultSopsFile = ./secrets/secrets.yaml;
+    defaultSopsFormat = "yaml";
+    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+    secrets."rancher/k3s/server/token" = {
+      # path = "/var/lib/rancher/k3s/server/token";
+    };
+  };
 
   nix = {
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+    gc.automatic = true;
   };
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
 
-  networking.hostName = meta.hostname; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  # networking.networkmanager.settings.connection."ipv4.method" = "auto";
+  networking = {
+    hostName = meta.hostname; # Define your hostname.
 
-  programs.zsh.enable = true; 
+    # Pick only one of the below networking options.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networkmanager.enable = true; # Easiest to use and most distros use this by default.
+    # networkmanager.settings.connection."ipv4.method" = "auto";
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ 80 ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    firewall.enable = false;
+  };
+
+  programs.zsh.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
@@ -54,25 +74,39 @@
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
-  services.k3s = {
-    enable = true;
-    role = "server";
-    tokenFile = /var/lib/rancher/k3s/server/token;
-    extraFlags = toString ([
-	    "--write-kubeconfig-mode \"0644\""
-	    "--cluster-init"
-	    "--disable servicelb"
-	    "--disable traefik"
-	    "--disable local-storage"
-    ] ++ (if meta.hostname == "homelab-0" then [] else [
-	      "--server https://homelab-0:6443"
-    ]));
-    clusterInit = (meta.hostname == "homelab-0");
-  };
+  services = {
+    k3s = {
+      enable = true;
+      role = "server";
+      tokenFile = config.sops.secrets."rancher/k3s/server/token".path;
+      extraFlags = toString ([
+          "--write-kubeconfig-mode \"0644\""
+          "--cluster-init"
+          "--disable servicelb"
+          "--disable traefik"
+          "--disable local-storage"
+        ]
+        ++ (
+          if meta.hostname == "homelab-0"
+          then []
+          else [
+	    # replace homelab-0 with IP address, if router does not support custom DNS (naughty ISP)
+            "--server https://homelab-0:6443"
+          ]
+        ));
+      clusterInit = meta.hostname == "homelab-0";
+    };
 
-  services.openiscsi = {
-    enable = true;
-    name = "iqn.2016-04.com.open-iscsi:${meta.hostname}";
+    openiscsi = {
+      enable = true;
+      name = "iqn.2016-04.com.open-iscsi:${meta.hostname}";
+    };
+
+    # Enable the OpenSSH daemon.
+    openssh = {
+      enable = true;
+      banner = "\n   _                 _     _ \n  | |_ ___ _____ ___| |___| |_\n  |   | . |     | -_| | .'| . |\n  |_|_|___|_|_|_|___|_|__,|___|\n\n Welcome to homelab.\n\n";
+    };
   };
 
   # Enable CUPS to print documents.
@@ -88,28 +122,29 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.taylor = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
     shell = pkgs.zsh;
     packages = with pkgs; [
       tree
       fastfetch
+      vim
     ];
     # Created using mkpasswd
     hashedPassword = "$6$QHI78ky1rOZZkAOh$FCRwbkcpLynrwzuQ1shI6q5s3xav7ipfp4voxWxNZM7SKR5ga7RWhcmWPpFfb0jmTXObd39mvG9I.h4n3XJZx1";
     openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDXAM2ekfY3J6RvvGzI7KgrmzOHUs/AuxS9+7yqppC87OKE6Gu7XlGukgvroyT1QGvvND9AB2YMd8ddGt96WzJnFWsKCVlu3n+3N4MeaM3bSXxouvc/Tse/dZ2noltzJ14WlQelOMgEpFBMUUd8pOOpAJrrpuF/7i44myrzqzEWTI1IdM60w6IXq6T0k/4nnBJNnpMiTSDBBdM+f5on2GCKwMB9NL05Elok32dLAnEIdFh+jlznq9fu+sMgAUeTnPAjibPwTNfrZN9dfzssp5oyz28K0Ffk17IJvwQlc5RjuBl5iCpd7ST2xDvpQsSSJdSJ7JE+/OeBTaA6R0bS6heBH+ojQgFJ+6NDlfoMSVSInnbSUWhK82D5zDJiQIdd/UXD9W6MDcDvPNa+/25qExhODl7Rx0EsI8zMTuQqM2gCfBt5inRiwCQrbECEDp6ROIP0gmJ+Z0CUytxDQznf3tN1WI/j+smlhyaazetaW3558E30DvrUHHSxq49/vEvmpE0= amaterasu access key"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB8wB5rMU+3zBLjdzq4E4ziK74XTyPEqLRoiMmI7UbJ4 taylor@amaterasu"
     ];
   };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-     neovim
-     k3s
-     cifs-utils
-     nfs-utils
-     git
-     dig
+    neovim
+    k3s
+    cifs-utils
+    nfs-utils
+    git
+    dig
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -121,16 +156,6 @@
   # };
 
   # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.banner = "\n   _                 _     _ \n  | |_ ___ _____ ___| |___| |_\n  |   | . |     | -_| | .'| . |\n  |_|_|___|_|_|_|___|_|__,|___|\n\n Welcome to homelab.\n\n";
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ 80 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
@@ -154,6 +179,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
-
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -6,9 +6,16 @@
     # Disko
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = { self, nixpkgs, disko, ... }@inputs: let
+  outputs = {
+    self,
+    nixpkgs,
+    disko,
+    sops-nix,
+    ...
+  } @ inputs: let
     nodes = [
       "homelab-0"
       "homelab-1"
@@ -16,21 +23,22 @@
     ];
   in {
     nixosConfigurations = builtins.listToAttrs (map (name: {
-	    name = name;
-	    value = nixpkgs.lib.nixosSystem {
-     	    specialArgs = {
-            meta = { hostname = name; };
+        name = name;
+        value = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            meta = {hostname = name;};
           };
           system = "x86_64-linux";
           modules = [
-              # Modules
-	            disko.nixosModules.disko
-	            ./hardware-configuration.nix
-	            ./disko-config.nix
-	            ./configuration.nix
-	          ];
+            # Modules
+            disko.nixosModules.disko
+            ./hardware-configuration.nix
+            ./disko-config.nix
+            sops-nix.nixosModules.sops
+            ./configuration.nix
+          ];
         };
-    }) nodes);
+      })
+      nodes);
   };
 }
-
